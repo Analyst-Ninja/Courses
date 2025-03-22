d2ab37730cec1e58ecff29d181127e3d9bf2739b
Analyst-Ninja <r.kumar01@hotmail.com>
2025-03-22 21:48:08 +0530

    langsmithAndPrompts

---
+++ 3.llm_chain.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+load_dotenv()
+
+# Initialized the LLM
+llm = GoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
+
+# Creating the prompt
+prompt = PromptTemplate(
+    template="Suggest a catchy blog title for the topic:- {topic}",
+    input_variables=['topic']
+)
+
+# Create a LLMChain
+chain = LLMChain(llm=llm, prompt=prompt)
+
+# Run the chain with specific topic
+topic = input("Enter the topic: ")
+output = chain.run(topic)
+
+print("Generated blog title: ",output)
+
