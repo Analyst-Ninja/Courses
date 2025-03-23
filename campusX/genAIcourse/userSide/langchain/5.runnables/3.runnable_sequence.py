db0da90ba72481ca216aace97b842731a2c47d17
Analyst-Ninja <r.kumar01@hotmail.com>
2025-03-23 13:24:06 +0530

    RunnablesPart2

---
+++ campusX/genAIcourse/userSide/langchain/5.runnables/3.runnable_sequence.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableSequence
+
+load_dotenv("../.env")
+
+prompt1 = PromptTemplate(
+    template="Write a joke about the topic: {topic}",
+    input_variables=['topic']
+)
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template="Explain the following joke: \n{text}",
+    input_variables=['text']
+)
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+print(chain.invoke({"topic" : "Python"}))
