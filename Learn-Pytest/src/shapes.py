a5475ebe2e39fd87eeae9dde78815658c7116b48
Rohit Kumar <r.kumar01@hotmail.com>
2025-06-07 12:04:23 +0530

    pytest fixtures

--- Learn-Pytest/src/shapes.py
+++ Learn-Pytest/src/shapes.py
@@ -21,3 +21,24 @@ class Circle(Shape):
 
     def perimeter(self):
         return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, length, breadth):
+        if length >= 0 and breadth >= 0:
+            self.length = length
+            self.breadth = breadth
+        else:
+            raise ValueError("Dimensions must be >=0")
+
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return False
+        else:
+            return self.length == other.length and self.breadth == other.breadth
+
+    def area(self):
+        return self.breadth * self.length
+
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
